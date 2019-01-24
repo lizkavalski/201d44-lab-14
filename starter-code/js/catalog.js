0aeb06fbd742ca81a60b3537627880df25471816
lizkavalski <lizkavalski@gmail.com>
2019-01-24 15:51:22 -0800

    finish catalog

--- starter-code/js/catalog.js
+++ starter-code/js/catalog.js
@@ -7,10 +7,10 @@ var cart = new Cart([]);
 
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
+var selectElement = document.getElementById('items');
 function populateForm() {
 
   //TODO: Add an <option> tag inside the form's select for each product
-  var selectElement = document.getElementById('items');
   for (var i in Product.allProducts) {
     var opEl = document.createElement('option');
     opEl.textContent= Product.allProducts[i].name;
@@ -35,18 +35,31 @@ function handleSubmit(event) {
 
 }
 
+
+// TODO: get the quantity
+
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
-  // TODO: get the quantity
+
   // TODO: using those, add one item to the Cart
+  cart.addItem(Product.allProducts[parseInt(selectElement.selectedIndex)].name,  parseInt(document.getElementById('quantity').value));
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+  var cartCount = document.getElementById('itemCount');
+  cartCount.textContent = `(${cart.items.length})`;
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
+  console.log(Product.allProducts[parseInt(selectElement.selectedIndex)].name);
+  var cartContents = document.getElementById('cartContents');
+  var cartItem = document.createElement('p');
+  cartItem.textContent= `${Product.allProducts[parseInt(selectElement.selectedIndex)].name}: ${ parseInt(document.getElementById('quantity').value)}`;
+  cartContents.appendChild(cartItem);
+   
   // TODO: Get the item and quantity from the form
   // TODO: Add a new element to the cartContents div with that information
 }
